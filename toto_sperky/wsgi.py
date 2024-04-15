d307e041ce4d5d02e04f9b7f3ac42428cfddba81
LukasMandik <swipe47@gmail.com>
2024-04-15 17:11:23 +0200

    add django

---
+++ toto_sperky/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for toto_sperky project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'toto_sperky.settings')
+
+application = get_wsgi_application()
---
+++ toto_sperky_web/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TotoSperkyWebConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'toto_sperky_web'
