d848fe1a775be3e6ffe238dd931aa7c7a6f8b62f
LukasMandik <swipe47@gmail.com>
2024-04-18 21:08:32 +0200

    connect vps

--- toto_sperky/settings.py
+++ toto_sperky/settings.py
@@ -155,3 +155,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+#test
