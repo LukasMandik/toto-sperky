785304e15cb22e4ba44735d81f7408130b8c13c1
LukasMandik <swipe47@gmail.com>
2024-09-26 13:19:23 +0200

    fix video enconding

--- toto_sperky/settings.py
+++ toto_sperky/settings.py
@@ -127,35 +127,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "root": {"level": "INFO", "handlers": ["file"]},
-    "handlers": {
-        "file": {
-            "level": "INFO",
-            "class": "logging.FileHandler",
-            "filename": os.path.join(BASE_DIR, "logs/django.log"),  # Zmena cesty
-            "formatter": "app",
-        },
-    },
-    "loggers": {
-        "django": {
-            "handlers": ["file"],
-            "level": "INFO",
-            "propagate": True
-        },
-    },
-    "formatters": {
-        "app": {
-            "format": (
-                u"%(asctime)s [%(levelname)-8s] "
-                "(%(module)s.%(funcName)s) %(message)s"
-            ),
-            "datefmt": "%Y-%m-%d %H:%M:%S",
-        },
-    },
-}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
