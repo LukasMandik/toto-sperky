b3d39d990d4228c9aaa93cac79869e31f1811ffb
LukasMandik <swipe47@gmail.com>
2024-10-12 12:54:13 +0200

    refactor code and any fix

--- static/js/script.js
+++ static/js/script.js
@@ -92,58 +92,6 @@ $(document).ready(function() {
 
 
 
-
-
-  
-  // $(document).ready(function() {
-  //   gsap.utils.toArray('.second_home_container').forEach((elem) => {
-  //       let lines = elem.querySelectorAll('.background_wrapper');
-  
-  //       lines.forEach((line) => {
-  //           let Timeline = gsap.timeline({
-  //               ease: "slowMo.ease",
-  //               scrollTrigger: {
-  //                   trigger: line,
-  //                   start: "top 100svh",
-  //                   end: "bottom 50svh",
-  //                   scrub: true,
-  //                   // markers: true,
-  //                   // toggleActions: "restart none none reverse",
-  //               }
-  //           });
-  
-  //           Timeline.from(line, { backgroundPositionX: "200", rotation:"120deg"});
-  //       });
-  //   });
-  // });
-
-  // $(document).ready(function() {
-  // // Inicializuj ScrollTrigger
-  // gsap.registerPlugin(ScrollTrigger);
-
-  // // Vytvor animáciu s ScrollTrigger
-  // gsap.to('.logo_dot', {
-  //   rotation: "-=360_cw",
-  //   transformOrigin: "7220px 2000px",
-  //   duration: 3,
-  //   ease: 'power2.inOut',
-  //   repeat: 100000,
-  //   scrollTrigger: {
-  //     trigger: '.starterdot', // Bod, kde sa spustí animácia
-  //     start: 'top 580svh',     // Začiatok triggeru (keď je 80% prvku v zornom poli)
-  //     end: 'bottom 190svh',    // Koniec triggeru
-  //     scrub: 1,             // Skrúbovací efekt pri posúvaní
-  //     toggleActions: 'play none none none', // Akcie pri spustení
-  //     markers: true,
-  //   },
-  // });
-
-  // });
-
-
-
-
-
   $(document).ready(function() {
     gsap.registerPlugin(ScrollTrigger);
   
@@ -174,28 +122,7 @@ $(document).ready(function() {
       
     });
   });
-  // $(document).ready(function() {
-  //   gsap.registerPlugin(ScrollTrigger);
   
-  //   gsap.utils.toArray('.footer_a').forEach((elem) => {
-  //     // Predpokladáme, že každý prvok má niečo ako čiaru alebo iný element na animovanie
-  //     let line = elem.querySelectorAll('.logo_dot'); // Prvok, ktorý budeme animovať
-  
-  //     // Vytvoríme GSAP časovú os
-  //     let timeline = gsap.timeline({
-  //       ease: "bounce",
-  //       repeat: 100, // Opakovanie nekonečne
-  //       yoyo: true, // Animácia sa opakuje opačným smerom
-  //     });
-  
-  //     // Pridáme animáciu na Timeline
-  //     timeline
-  //       .from(line[0], { rotation: "-=360_cw", transformOrigin: "7220px 2000px", duration: 260 }, 0)
-  //       .from(line[1], { rotation: "-=360_cw", transformOrigin: "7620px 2000px", duration: 527 }, 0)
-  //       .from(line[2], { rotation: "-=360_cw", transformOrigin: "-5370px -4250px", duration: 211 }, 0)
-  //       .from(line[3], { rotation: "-=360_cw", transformOrigin: "4020px 6220px", duration: 220}, 0);
-  //   });
-  // });
   
 
   $(document).ready(function() {
@@ -251,32 +178,6 @@ $(document).ready(function() {
         .to(line, { strokeDashoffset: 0 })
     });
   });
-  // $(document).ready(function() {
-  //   gsap.registerPlugin(ScrollTrigger);
-  
-  //   gsap.utils.toArray('.line_center_detail_product').forEach((elem) => {
-  //     let line = elem.querySelector('.line');
-  //     let lineLength = line.getTotalLength();
-  
-
-  //     line.style.strokeDasharray = lineLength;
-  //     line.style.strokeDashoffset = lineLength; 
-
-  //     let Timeline = gsap.timeline({
-  //       scrollTrigger: {
-  //         trigger: elem,
-  //         easy:"linear",
-  //         start: "top 700svh",
-  //         end: "bottom 700svh",
-  //         scrub: false,
-  //         // markers: true,
-  //         toggleActions: "restart none none reverse",  
-  //       }
-  //     });
-  //     Timeline
-  //       .to(line, { strokeDashoffset: 0 })
-  //   });
-  // });
   $(document).ready(function() {
     gsap.registerPlugin(ScrollTrigger);
   
@@ -432,62 +333,6 @@ $(document).ready(function() {
       });
     });
   });
-
-
-//   $(document).ready(function() {
-//     gsap.utils.toArray('.gallery_container .container').forEach((elem) => {
-//         let lines = elem.querySelectorAll('.box a');
-  
-//         let Timeline = gsap.timeline({
-//             scrollTrigger: {
-//                 trigger: elem,
-//                 start: "center 100svh",
-//                 end: "center 100svh",
-//                 scrub: false,
-//                 toggleActions: "restart none none reverse",
-//                 markers: true,
-//             }
-//         });
-  
-//         lines.forEach((line) => {
-//             Timeline.to(line, { className: '+=hover' }, 0.5); // pridanie triedy 'hover'
-//         });
-//     });
-// });
-
-
-  
-// $(document).ready(function() {
-//   // Funkcia na detekciu šírky obrazovky
-//   function isMobileScreen() {
-//       return window.matchMedia("(max-width: 768px)").matches;
-//   }
-
-//   if (isMobileScreen()) {
-//       let links = gsap.utils.toArray('.box');
-
-//       links.forEach((link) => {
-//           link.addEventListener('click', function(event) {
-//               if (!link.classList.contains('hover')) {
-//                   event.preventDefault(); // Zabraňuje predvolenému správaniu (navigácia)
-//                   links.forEach(l => l.classList.remove('hover')); // Odstráni 'hover' z ostatných prvkov
-//                   link.classList.add('hover'); // Pridá triedu 'hover'
-//               } else {
-//                   link.classList.remove('hover'); // Odstráni triedu 'hover' pri druhom kliknutí
-//               }
-//           });
-//       });
-
-//       window.addEventListener('scroll', function() {
-//           links.forEach((link) => {
-//               link.classList.remove('hover'); // Odstráni triedu 'hover' pri skrolovaní
-//           });
-//       });
-//   }
-// });
-
-
-
   $(document).ready(function() {
     gsap.utils.toArray('.text-about-me1').forEach((elem) => {
         let lines = elem.querySelectorAll('.text-about-me1 p');
@@ -615,91 +460,6 @@ $(document).ready(function() {
       });
     });
   });
-  // $(document).ready(function() {
-
-  //   gsap.set(".box2", {
-  //     x: (i) => i * 155
-  //   });
-    
-    
-  //   gsap.to(".box2", {
-  //     duration: 40,
-  //     ease: "linear",
-  //     x: "+=775", //move each box 500px to right
-  //     modifiers: {
-  //       x: gsap.utils.unitize(x => parseFloat(x) % 775) //force x value to be between 0 and 500 using modulus
-  //     },
-  //     repeat: -1
-  //   });
-  // });
-
-
-  // $(document).ready(function() {
-
-
-  //     gsap.utils.toArray('.startermenu').forEach((elem) => {
-  //         let lines = elem.querySelectorAll('.main_navbar .menu ul li a');
-    
-  //         let Timeline = gsap.timeline({
-  //           scrollTrigger: {
-  //               trigger: elem,
-  //               start: "botton 600svh",
-  //               end: "top 600svh",
-  //               scrub: false,
-  //               toggleActions: "restart none none reverse",
-  //               markers: true,
-  //           }
-  //         });
-      
-  //         lines.forEach((line) => {
-  //           Timeline.to(line, { x: 40,duration: 0.35 });
-  //         });
-  //       });
-    
-  // });
-
-
-
-  
-  // $(document).on("click", ".hamburger", function() {
-  //   if ($(".menu").hasClass("active")) {
-  //     gsap.utils.toArray('.startermenu').forEach((elem) => {
-  //       let lines = elem.querySelectorAll('.main_navbar .menu ul li');
-     
-  //       let Timeline = gsap.timeline({
-  //         scrollTrigger: {
-  //           trigger: elem,
-  //           start: "botton 600svh",
-  //           end: "top 600svh",
-  //           scrub: false,
-  //           toggleActions: "play none none reverse",
-  //           // markers: true,
-  //         }
-  //       });
-  //         Timeline.from(lines, { x: 40, stagger: 0.05});
-  //       });
-  //   }
-  // });
-    
-  // $(document).on("click", ".hamburger", function() {
-  //   if ($(".menu").hasClass("active")) {
-  //     gsap.utils.toArray('.startermenu').forEach((elem) => {
-  //       let lines = elem.querySelectorAll('.main_navbar .menu ul li');
-     
-  //       let Timeline = gsap.timeline({
-  //         scrollTrigger: {
-  //           trigger: elem,
-  //           start: "botton 600svh",
-  //           end: "top 600svh",
-  //           scrub: false,
-  //           toggleActions: "play none none reverse",
-  //           // markers: true,
-  //         }
-  //       });
-  //         Timeline.from(lines, { x: 40, stagger: 0.05});
-  //       });
-  //   }
-  // });
   
   $(document).on("click", ".hamburger", function() {
     if ($(".menu").hasClass("active")) {
@@ -760,26 +520,6 @@ gsap.from(Container, {
 });
 }
 });
-// $(document).on("click", ".hamburger", function() {
-//   if ($(".menu").hasClass("active")) {
-//   const Container = document.querySelectorAll('.switch-box');
-// // Create an animation loop using GSAP
-// gsap.from(Container, {
-//   x: -340,
-//   delay: 0.3, 
-//   opacity: 0, 
-//   // y: 2,
-//   // rotate: 1,
-//   stagger: 0.03,  
-//   duration: 0.6, 
-//   // repeat: -1,
-//   // yoyo: true, 
-//   ease: 'power2.inOut' // Smooth easing function
-// });
-// }
-// });
-
-
   
   $(document).ready(function() {
   
@@ -809,22 +549,6 @@ gsap.from(Container, {
     });
   });
 
-// $(document).ready(function() {
-//   const svgContainer = document.querySelector('.svg_container');
-
-// // Create an animation loop using GSAP
-// gsap.to(svgContainer, {
-//   x: 10, // Move horizontally by 10 pixels
-//   // y: 2,
-//   // rotate: 1,  
-//   duration: 2, // Animation duration in seconds
-//   repeat: -1, // Repeat indefinitely
-//   yoyo: true, // Reverse the animation direction back and forth
-//   ease: 'power2.inOut' // Smooth easing function
-// });
-// });
-
-
 $(document).ready(function() {
   gsap.utils.toArray('.starter2').forEach((elem) => {
       let lines = elem.querySelectorAll('.main_title_container h1 ,.main_title_container h2');
@@ -845,32 +569,6 @@ $(document).ready(function() {
     });
   });
 });
-
-
-
-
-// $(document).ready(function() {
-//   gsap.registerPlugin(ScrollTrigger);
-//   const title = ".sluzby_container .title_home_container h2";
-  
-
-//   gsap.from(title,{
-//       duration: 2,
-//       x:100,
-//       stagger: 0.25,
-//       opacity: 0,
-//       ease: "power2.out",
-//       scrollTrigger: {
-//         trigger: title,
-//         end: "center 500svh",
-//         start: "center 700svh",
-//         // markers: true,
-//         scrub: false,
-//         toggleActions: "restart none none reverse",
-//         // play pause resume reverse restart reset complete
-//     }
-// })  
-// })
 
 $(document).ready(function() {
   gsap.utils.toArray('.starter').forEach((elem) => {
@@ -893,28 +591,6 @@ $(document).ready(function() {
   });
 });
 
-
-// $(document).ready(function() {
-//   gsap.utils.toArray('.container_home').forEach((elem) => {
-//       let lines = elem.querySelectorAll('.container_home .box_home');
-
-//       lines.forEach((line) => {
-//           let Timeline = gsap.timeline({
-//               ease: "elastic",
-//               scrollTrigger: {
-//                   trigger: line,
-//                   start: "top 550svh",
-//                   end: "bottom 600svh",
-//                   scrub: false,
-//                   // markers: true,
-//                   toggleActions: "restart none none reverse",
-//               }
-//           });
-
-//           Timeline.from(line, { opacity: 0, y: 20 });
-//       });
-//   });
-// });
 
 $(document).ready(function() {
   gsap.utils.toArray('.starter').forEach((elem) => {
@@ -962,43 +638,6 @@ $(document).ready(function() {
 });
 
 
-// $(document).ready(function() {
-//   gsap.utils.toArray('.starter').forEach((elem) => {
-//       let lines = elem.querySelectorAll('.main_image_container img');
-
-//       lines.forEach((line) => {
-//           let Timeline = gsap.timeline({
-//               ease: "ease.inOut",
-//               scrollTrigger: {
-//                   trigger: line,
-//                   start: "top 50svh",
-//                   end: "bottom -500svh",
-//                   scrub: true,
-//                   // markers: true,
-//                   // toggleActions: "restart none none reverse",
-//               }
-//           });
-
-//           // Timeline.to(line, {width: 270, y: 350 , rotate: -3},0,0)
-//           // Timeline.to(line, {  });
-//           Timeline.to(line, { width: 330, y: 380})
-//           // Timeline.to(line, {  });
-//       });
-//   });
-// });
-
-
-
-
-
-
-    
-  
-
-      
-
-      
-
 document.addEventListener('DOMContentLoaded', () => {
     const currentPath = window.location.pathname;
     const menuItems = document.querySelectorAll('.startermenu li a');
@@ -1014,15 +653,6 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   if ('ontouchstart' in window || navigator.maxTouchPoints) {
-//       document.querySelectorAll('.container .box').forEach(function(box) {
-//           box.classList.remove('hover');
-//       });
-//   }
-// });
-
 
 document.addEventListener("DOMContentLoaded", function() {
   const customizeCookiesButton = document.getElementById("customizeCookies");
--- toto_sperky/settings.py
+++ toto_sperky/settings.py
@@ -29,11 +29,11 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*","localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".totosperky.sk", "192.168.100.4"]
 
 CSRF_TRUSTED_ORIGINS = [
     # 'https://blog.hubspot.com/website/what-is-localhost',  # Povoliť localhost
-    # 'https://*.vasedomena.com',  # Povoliť všetky poddomény `vasedomena.com` s protokolom HTTPS
+    'https://*.totosperky.sk',  # Povoliť všetky poddomény `vasedomena.com` s protokolom HTTPS
     'http://127.0.0.1:8000',  # Povoliť konkrétnu IP adresu a port
     'http://37.205.12.97',
 ]
