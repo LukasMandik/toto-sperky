0929141e2057f7dd03df28e7ce96ce0d072a60f9
LukasMandik <swipe47@gmail.com>
2024-10-14 10:25:49 +0200

    edit sitemaps.py

--- toto_sperky_web/sitemaps.py
+++ toto_sperky_web/sitemaps.py
@@ -5,26 +5,35 @@ from .models import Blog, Product, Category
 class BlogSitemap(Sitemap):
     changefreq = "weekly"
     priority = 0.9
+    protocol = 'https'
 
     def items(self):
         return Blog.objects.filter(available=True)
 
     def lastmod(self, obj):
         return obj.updated
+    
+    def location(self,obj):
+        return '/blog/%s' % (obj.slug)
 
 class ProductSitemap(Sitemap):
     changefreq = "monthly"
     priority = 0.8
+    protocol = 'https'
 
     def items(self):
         return Product.objects.filter(available=True)
 
     def lastmod(self, obj):
         return obj.updated
+    
+    def location(self,obj):
+        return '/product/%s' % (obj.slug)
 
 class CategorySitemap(Sitemap):
     changefreq = "monthly"
     priority = 0.7
+    protocol = 'https'
 
     def items(self):
         return Category.objects.all()
@@ -34,15 +43,14 @@ class CategorySitemap(Sitemap):
 
 class StaticViewSitemap(Sitemap):
     changefreq = "monthly"
-    priority = 0.5
+    priority = 0.8
+    protocol = 'https'
 
     def items(self):
         return [
             'toto_sperky_web:home',
-            'toto_sperky_web:gallery',
-            'toto_sperky_web:blog',
             'toto_sperky_web:about_me',
-            'toto_sperky_web:cookies'
+            'toto_sperky_web:contact',
         ]
 
     def location(self, item):
