8dee1f40c9c50fc4f2b4ee2a716e423eff5af4cc
LukasMandik <swipe47@gmail.com>
2024-10-23 14:14:46 +0200

    test loading js

--- static/js/loading.js
+++ static/js/loading.js
@@ -1,46 +1,37 @@
-
-
-
-
-function mediaLoaded() {
-    const images = document.getElementsByTagName("img");
-    const videos = document.getElementsByTagName("video");
-
-    for (const img of images) {
-      if (!img.complete) {
-        return false;
-      }
+function imagesLoaded() {
+  const images = document.getElementsByTagName("img");
+  for (const img of images) {
+    if (!img.complete) {
+      return false;
     }
-
-    for (const video of videos) {
-      if (video.readyState < 4) { // HAVE_ENOUGH_DATA
-        return false;
-      }
-    }
-
-    return true;
+  }
+  return true;
 }
 
 function hideLoadingScreen() {
-    const loadingScreen = document.getElementById("loading-screen");
-    if (mediaLoaded()) {
-      loadingScreen.style.opacity = "0";
-      setTimeout(function () {
-        loadingScreen.style.display = "none";
-      }, 500);
-    } else {
-      setTimeout(hideLoadingScreen, 100);
-    }
+  const loadingScreen = document.getElementById("loading-screen");
+  if (imagesLoaded()) {
+    // loadingScreen.style.opacity = "0";
+    setTimeout(function () {
+      loadingScreen.style.display = "none";
+    }, 500);
+  } else {
+    setTimeout(hideLoadingScreen, 100);
+  }
 }
 
 window.addEventListener("load", function () {
-    hideLoadingScreen();
+  hideLoadingScreen();
 });
 
+
+
+
+
 window.addEventListener("load", function () {
-    const loadingScreen = document.getElementById("loading-screen");
-    loadingScreen.style.opacity = "0";
-    setTimeout(function () {
-        loadingScreen.style.display = "none";
-    }, 1000);
+const loadingScreen = document.getElementById("loading-screen");
+// loadingScreen.style.opacity = "0";
+setTimeout(function () {
+loadingScreen.style.display = "none";
+}, 1000);
 });
